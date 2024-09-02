ec5e2abbca2533196f56fd84093ef193ee8762fd
Leo Boulatrous <lboulatr@z1r4p4.42lyon.fr>
2024-09-02 11:19:14 +0200

    feat: remove get_room_id fct

--- src/ants/move_ants.c
+++ src/ants/move_ants.c
@@ -1,5 +1,8 @@
 #include "lem_in.h"
 
+static bool     is_next_room_empty(t_lem_in *lem_in, char *room_name);
+static void     move_ants(t_lem_in *lem_in);
+
 void    move_ants_manager(t_lem_in *lem_in)
 {
     display_data(lem_in);
@@ -9,59 +12,33 @@ void    move_ants_manager(t_lem_in *lem_in)
     move_ants(lem_in);
 }
 
-static bool is_next_room_empty(t_lem_in *lem_in, int room_id)
+static bool is_next_room_empty(t_lem_in *lem_in, char *room_name)
 {
-    if (lem_in->rooms[room_id].is_empty)
-        return (true);
+    for (int i = 0; i < lem_in->n_rooms; i++)
+    {
+        if (ft_strncmp(lem_in->rooms[i].name, room_name, ft_strlen(room_name)) == 0)
+        {
+            if (lem_in->rooms[i].is_empty)
+                return (true);
+            else
+                return (false);
+        }
+    }
     return (false);
 }
 
-static int get_room_id(t_lem_in *lem_in, char *room_name)
+
+static void    move_ants(t_lem_in *lem_in)
 {
-    int i;
-
-    i = 0;
-    while (i < lem_in->n_rooms)
+    for (int i = 0; i < lem_in->n_ants; i++)
     {
-        if (ft_strncmp(lem_in->rooms[i].name, room_name, ft_strlen(room_name)) == 0)
-            return (lem_in->rooms[i].id);
-        i++;
-    }
-    return (-1);
-}
-
-void    move_ants(t_lem_in *lem_in)
-{
-    int         i;
-    size_t      j;
-    int         room_id;
-
-    for (int c = 0; c < 1; c++)
-    {
-        i = 0;
-        while (i < lem_in->n_ants)
+        for (size_t j = 0; j < lem_in->ants[i].room->links.size; j++)
         {
-            j = 0;
-            while(j < lem_in->ants[i].room->links.size)
+            printf("ant %d\n", i);
+            if (is_next_room_empty(lem_in, lem_in->ants[i].room->links.arr[j]))
             {
-                room_id = get_room_id(lem_in, lem_in->ants[i].room->links.arr[j]);
-                if (room_id == -1)
-                    return ;
-                if (is_next_room_empty(lem_in, room_id))
-                {
-                    printf("ROOM_ID = %d\n", room_id);
-                    lem_in->ants[i].room = &lem_in->rooms[room_id];
-                    lem_in->rooms[room_id].is_empty = false;
-                    lem_in->rooms[lem_in->ants[i].room->id].is_empty = true;
-                    display_info_rooms_status(lem_in);
-                }
-                j++;
+                printf("next empty room name = %s\n", lem_in->ants[i].room->links.arr[j]);
             }
-            i++;
         }
     }
-
-    printf("After move\n");
-    display_info_rooms_status(lem_in);
-    display_info_ants(lem_in);
 }
